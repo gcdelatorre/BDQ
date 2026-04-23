a4e862d549d1cdc0a878ae53694da06359695f7b
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-04-23 23:32:47 +0800

    add date of  birth for employee

--- bdq_schema.sql
+++ bdq_schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `first_name`       VARCHAR(50)        NOT NULL,
   `middle_name`      VARCHAR(50)        DEFAULT NULL,
   `last_name`        VARCHAR(50)        NOT NULL,
+  `date_of_birth`    DATE               DEFAULT NULL,
   `role`             ENUM('Nurse','Midwife','Admin','Assistant') NOT NULL,
   `username`         VARCHAR(30)        NOT NULL UNIQUE,
   `password_hash`    VARCHAR(255)       NOT NULL,
